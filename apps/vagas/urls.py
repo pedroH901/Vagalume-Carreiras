5b15bdab8465f95d7319ad37246f970e97f0baac
Pedro <phdf2016@gmail.com>
2025-11-13 20:44:02 -0300

    tentativa 1 de corrigir bug de ver candidato

--- apps/vagas/urls.py
+++ apps/vagas/urls.py
@@ -11,11 +11,9 @@ urlpatterns = [
     path('vagas/aplicar/<int:vaga_id>/', views.aplicar_vaga, name='aplicar_vaga'),
     path('dashboard/candidato/', views.home_candidato, name='home_candidato'),
     path('dashboard/recrutador/', views.home_recrutador, name='home_recrutador'),
-    path('perfil_empresa/', views.perfil_empresa, name='perfil_empresa'),
     path('vagas/<int:vaga_id>/candidatos/', views.ver_candidatos_vaga, name='ver_candidatos_vaga'),
     path('radar-de-talentos/', views.radar_de_talentos, name='radar_de_talentos'),
-
     path('perfil_empresa/', views.perfil_empresa, name='perfil_empresa'),
     path("planos_empresa/", views.planos_empresa, name="planos_empresa"),
-
+    path('painel-admin/', views.painel_admin, name='painel_admin'),
 ]
