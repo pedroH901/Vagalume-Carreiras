01e6328a781f9aaab72a8167b2f333fbea48cb89
gabriel-wav <gabfer.silva@gmail.com>
2025-11-17 21:29:59 -0300

    alterado footer e feito correcoes em outras paginas

--- apps/matching/apps.py
+++ apps/matching/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
+
 class MatchingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.matching' 
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.matching"
